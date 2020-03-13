3ceba9cc0d924fceee50e842858102bc04eb9389
egconley <31393715+egconley@users.noreply.github.com>
2020-03-13 15:23:46 -0700

    deleted sout

--- app/src/main/java/com/econley_hle_rvaknin/stopwatch/RecyclerViewAdapter.java
+++ app/src/main/java/com/econley_hle_rvaknin/stopwatch/RecyclerViewAdapter.java
@@ -31,7 +31,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.mItem = mValues.get(position);
-        System.out.println("mValues = " + mValues);
         holder.mTitleView.setText(mValues.get(position));
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
