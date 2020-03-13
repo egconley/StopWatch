ef5c794e765dc8f82b69db993ea2a975b5db1e0f
RAN VAKNIN <ranvaknin0@gmail.com>
2020-03-13 11:40:16 -0700

    icon added and apk built

--- app/src/main/java/com/econley_hle_rvaknin/stopwatch/RecyclerViewAdapter.java
+++ app/src/main/java/com/econley_hle_rvaknin/stopwatch/RecyclerViewAdapter.java
@@ -1,28 +1,18 @@
 package com.econley_hle_rvaknin.stopwatch;
 
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Address;
 import android.location.Geocoder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.econley_hle_rvaknin.stopwatch.bottomnavigation.RecentDestinationsFragment;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.IOException;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
+
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link } and makes a call to the
